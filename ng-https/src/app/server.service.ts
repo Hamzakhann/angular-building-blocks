96b7475fd042c968dcdb96cc291cbfe614d99bb0
Hamzakhann <hamzakhann66@gmail.com>
2019-01-29 13:53:11 +0500

    half http

---
+++ ng-https/src/app/server.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Headers , Http} from '@angular/http';
+
+@Injectable()
+export class ServerService {
+  constructor(private http:Http){}
+
+  storeServers(servers:any[]){
+    const headers = new Headers({
+      'Content-Type' : 'application/json'
+    })
+    return this.http.post('https://maria-project-presentation.firebaseio.com/data.json' 
+    , servers
+    , {headers:headers}
+    )
+  }
+}
